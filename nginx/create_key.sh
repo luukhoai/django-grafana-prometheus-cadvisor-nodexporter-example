10963b8a0644f6740b8b88af8d4cd50df1e897a1
khoailv <khoailv@evolableasia.vn>
2018-07-24 15:32:24 +0700

    update nginx

--- nginx/create_key.sh
+++ nginx/create_key.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /home/silverlight/Projects/test_django/nginx/nginx.key -out /home/silverlight/Projects/test_django/nginx/nginx.crt
+
+## Create username password
+sudo sh -c "echo -n 'admin:' >> /home/silverlight/Projects/test_django/nginx_dashboard/.htpasswd"
+sudo sh -c "openssl passwd -apr1 >> /home/silverlight/Projects/test_django/nginx_dashboard/.htpasswd"
